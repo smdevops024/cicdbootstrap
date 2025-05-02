6e7d68ee89921df6ed0fe3f18856b4fd6c6e9df7
Raja Muhammad Awais <smdevops24@gmail.com>
2025-05-02 15:18:11 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 echo "[INFO] Updating cicd-bootstrap.sh..."
 curl -s -o "$HOME/bin/cicd-bootstrap.sh" https://your-server.com/path/to/cicd-bootstrap-v1.0.7.sh
 chmod +x "$HOME/bin/cicd-bootstrap.sh"
-echo "[SUCCESS] Updated to v1.0.7"
+echo "[SUCCESS] Updated to v2.6"
